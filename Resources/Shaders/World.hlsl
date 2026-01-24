a7d3b1bc3e4bc2e401e6a5b6e3e27d20d87c9494
InfiniteC0re <inf.core0@gmail.com>
2026-01-25 01:15:35 +0500

    Update world shader to render water properly

--- Resources/Shaders/World.hlsl
+++ Resources/Shaders/World.hlsl
@@ -8,9 +8,6 @@ struct VS_IN
 
 struct PS_IN
 {
-    // float4 normal : NORMAL;
-    // float4 color : COLOR;
-    // float2 texcoord : TEXCOORD0;
     float4 position : SV_POSITION;
     float4 color : COLOR;
     float2 texcoord : TEXCOORD;
@@ -22,14 +19,29 @@ cbuffer ConstantBuffer : register(b0)
 	float4 cb_TexCoordOffsetAndAlpha;
 	float4 cb_AmbientColor;
 	float4 cb_ShadowColor;
+	float cb_IsWater;
+	float cb_IsLit;
 };
 
 PS_IN vs_main(VS_IN input)
 {
     PS_IN output;
 
+	// Calculate vertex screen position
     output.position = mul(float4(input.position, 1.0f), cb_matMVP);
-    output.color = lerp(cb_ShadowColor, cb_AmbientColor, input.color);
+    
+	// Calculate vertex color based on current lighting settings and shadow factor (stored as vertex data)
+	output.color.xyz = lerp(cb_ShadowColor.xyz, cb_AmbientColor.xyz, input.color.xyz);
+    
+	// Adjust water color
+	// Water recieves less ambient color
+	output.color.xyz = (input.color.xyz * cb_IsWater * 0.75f + output.color.xyz * 0.25f * cb_IsWater) + (output.color.xyz * cb_IsLit);
+
+	// Calculate opacity
+	// Used for water rings and probably something else
+	output.color.w = (input.color.x * cb_IsWater) + (1.0f * cb_IsLit);
+	
+	// Animate UV
     output.texcoord = input.texcoord + cb_TexCoordOffsetAndAlpha.xy;
 
     return output;
@@ -40,13 +52,12 @@ SamplerState albedo_texture_sampler : register(s0);
 
 float4 ps_main(PS_IN input) : SV_TARGET
 {
-    float4 tex_color = albedo_texture.Sample(albedo_texture_sampler, input.texcoord);
-    
-	tex_color.a *= cb_TexCoordOffsetAndAlpha.z;
+    float4 tex_color = albedo_texture.Sample(albedo_texture_sampler, input.texcoord) * input.color * cb_TexCoordOffsetAndAlpha.z;
 	
 #ifdef ALPHAREF
+	// The only alpharef value used by the game is 128 (0.5f)
     if (tex_color.a < 0.5f) discard;
 #endif
 
-    return tex_color * input.color;
+    return tex_color;
 }
